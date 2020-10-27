c643d8a6322fc37022dc97bbf301ded4245ff019
ThatcherK <kaliithatmau@gmail.com>
2020-10-27 10:41:15 +0300

    Add edit functionality

--- designhubcheckin/checkin/admin.py
+++ designhubcheckin/checkin/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Visitors
+from .models import Visitors, Register
 
 admin.site.register(Visitors)
+admin.site.register(Register)
--- designhubcheckin/checkin/views.py
+++ designhubcheckin/checkin/views.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.http import HttpResponse
 from .forms import LoginForm, SearchForm, AddForm, EditVisitorForm
-from .models import Visitors
-# Create your views here.
+from .models import Visitors, Register
 
 
 
@@ -16,7 +15,7 @@ def index(request):
     return render(request,'checkin/index.html',context)
 
 def login(request):
-    visitors_list = Visitors.objects.all()
+    visitors_list = Register.objects.all()
     search_form = SearchForm()
     add_form = AddForm()
     if request.method == 'POST':
@@ -52,21 +51,21 @@ def search(request):
                 return render(request, 'checkin/visitors.html',{'visitor':visitor, 'editform': editing_form})
 
 def add_new_visitor(request):
-    visitors_list = Visitors.objects.all()
+    visitors_list = Register.objects.all()
     search_form = SearchForm()
     add_form = AddForm()
     if request.method == 'POST':
         form = AddForm(request.POST)
-        print(form)
         if form.is_valid():
             name = form.cleaned_data.get('name')
             company = form.cleaned_data.get('company')
             temperature = form.cleaned_data.get('temperature')
             identification_number = form.cleaned_data.get('identification_number')
             telephone_number = form.cleaned_data.get('telephone_number')
-            # date = form.cleaned_data.get('date')
-            visitor = Visitors(name=name, company=company,temperature=temperature, identification_number=identification_number,telephone_number=telephone_number)
+            visitor = Visitors(name=name, company=company, identification_number=identification_number,telephone_number=telephone_number)
             visitor.save()
+            registered_visitor = Register(visitor=visitor, temperature=temperature)
+            registered_visitor.save()
             return render(request,'checkin/mainpage.html',{'form': search_form, 'addform':add_form, 'visitors_list':visitors_list})
 
         else:
@@ -75,18 +74,19 @@ def add_new_visitor(request):
             return HttpResponse("error")
 
 def check_in_returning_visitor(request, visitor_id):
+    visitors_list = Register.objects.all()
+    search_form = SearchForm()
+    add_form = AddForm()
     if request.method == 'POST':
         form = EditVisitorForm(request.POST)
         
         if form.is_valid():
-            print(form.cleaned_data.get('date'))
             visitor = Visitors.objects.get(pk=visitor_id)
             if visitor is not None:
-                date = form.cleaned_data.get('date')
                 temperature = form.cleaned_data.get('temperature')
-                visitor.date = date
-                visitor.temperature = temperature
-                visitor.save()
+                registered_visitor = Register.objects.get(visitor=visitor)
+                registered_visitor.temperature = temperature
+                registered_visitor.save()
                 return render(request,'checkin/mainpage.html',{'form': search_form, 'addform':add_form, 'visitors_list':visitors_list})
         else:
             form = EditVisitorForm()
